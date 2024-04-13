83d77ef8e4fc7310ac8973284cda594015ac6164
Guilherme Correia <gtavares739@gmail.com>
2024-04-13 03:27:10 -0300

    APP 03 - Times Numbers

---
+++ times-numbers/App.js
@@ -0,0 +1,13 @@
+import { View } from 'react-native'
+
+import Multiplier from './src/pages/Multiplier';
+
+function App(){
+  return(
+    <View>
+      <Multiplier />
+    </View>
+  )
+}
+
+export default App
